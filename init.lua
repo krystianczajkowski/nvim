8154ee53efa4ce49717229d7ac473e02b5063896
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2025-03-03 09:11:22 +0100

    fix: typo in one package name

--- init.lua
+++ init.lua
@@ -141,7 +141,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   { 'mbbill/undotree' },
-  { 'tpoe/vim-fugitive' },
+  { 'tpope/vim-fugitive' },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
